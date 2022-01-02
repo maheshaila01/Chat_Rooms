3d573618385dd2b2727bb9e7c59ab5f5c232b4e6
maheshaila01 <maheshbabu09.amb@gmail.com>
2022-01-02 22:11:18 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 dotenv.config();
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send("API is running");
+});
+
+app.get('/api/chat',(req,res) => {
+     res.send(chats);
 });
 
 const PORT = process.env.PORT || 5000;
